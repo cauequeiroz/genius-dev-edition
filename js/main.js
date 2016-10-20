6badb6c85d690a22ec1e32cc6ca7ce63a5905a7d
Caue Queiroz <cauenqueiroz@gmail.com>
2016-10-20 19:53:08 -0200

    Finish all key mapping

--- js/main.js
+++ js/main.js
@@ -9,11 +9,13 @@ var level = {
 
 var game = {
     allowedKeys: {
-        81: 'q',
-        87: 'w',
-        69: 'e',
-        82: 'r',
-        84: 't'
+        81: 'q', 87: 'w', 69: 'e', 82: 'r',
+        84: 't', 89: 'y', 85: 'u', 73: 'i',
+        79: 'o', 80: 'p', 65: 'a', 83: 's',
+        68: 'd', 70: 'f', 71: 'g', 72: 'h',
+        74: 'j', 75: 'k', 76: 'l', 90: 'z',
+        88: 'x', 67: 'c', 86: 'v', 66: 'b',
+        78: 'n', 77: 'm'
     },
 
     position: 0,
@@ -23,17 +25,17 @@ var game = {
             rqd = level.list[level.current][game.position];
 
         if ( key === rqd ) {
-            game.position++;
-            console.log('acertou!');
+            // console.log('acertou!');
+            game.position++;            
 
             if ( game.position === level.list[level.current].length ) {
-                console.log('Finish level!');
+                // console.log('Finish level!');
                 game.position = 0;
                 level.current++;
             }
         } else {
-            game.position = 0;
-            console.log('errou!');
+            // console.log('errou!');
+            game.position = 0;            
         }
     },
 
